4cbfa38991a2be0f721d4d03fcecd664971d9d89
Jakub Gawęda <kubag98@hotmail.com>
2022-04-23 17:23:33 +0200

    Create base db connection

--- OfficeReservationService/Program.cs
+++ OfficeReservationService/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+using OfficeReservationService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add dbservice
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlite("Data source = OfficeReservationDatabase.db"));
+// Setup IdentityCore
+builder.Services.AddIdentityCore<IdentityUser>(opt => opt.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DataContext>();
+
 
 var app = builder.Build();
 
