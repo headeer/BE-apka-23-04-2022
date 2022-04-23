d28775f5c9c034c3a951811ea3999bf86097b97d
Jakub Gawęda <kubag98@hotmail.com>
2022-04-23 20:02:14 +0200

    Set rules in DataContext

--- OfficeReservationService/Data/DataContext.cs
+++ OfficeReservationService/Data/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OfficeReservationService.Contracts.Models;
 
 namespace OfficeReservationService.Data
 {
@@ -8,6 +9,28 @@ namespace OfficeReservationService.Data
         public DataContext(DbContextOptions<DataContext> options)
             : base(options)
         {
+        }
+        public Company Company{ get; set; }
+        public Room Room{ get; set; }
+        public Seating Seating{ get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Company>()
+                .HasMany(x => x.Users)
+                .WithOne(x => x.Company)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Company>()
+                .HasMany(x => x.Rooms)
+                .WithOne(x => x.Company)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Room>()
+                .HasMany(x=>x.Seatings)
+                .WithOne(x=>x.Room)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
