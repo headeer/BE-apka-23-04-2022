2c0d0928c44fe9c653862b3a6320b1f114d56b04
Szymon Szałata <50450702+virouzrx@users.noreply.github.com>
2022-04-23 16:25:39 +0200

    Update WeatherForecastController.cs
    Test commit from GitHub Desktop

--- OfficeReservationService/OfficeReservationService/Controllers/WeatherForecastController.cs
+++ OfficeReservationService/OfficeReservationService/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace OfficeReservationService.Controllers
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+        sad
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
